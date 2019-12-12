56d8d795b27c8d2a882c25c82b61bd00dac3f96b
Tizian Hammerschmid <tizian.hammerschmid@htlstp.at>
2019-12-12 09:01:14 +0100

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <stdlib.h>
+#includes"header.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
+    cout << "geht des" << endl;
     return 0;
 }
